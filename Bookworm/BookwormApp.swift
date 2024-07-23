4ee22328e33d3a1693f60785be93889feba9b758
Justine Kenji Dela Cruz <kenjidckenji@gmail.com>
2024-07-23 15:39:30 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kenji Dela Cruz on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
